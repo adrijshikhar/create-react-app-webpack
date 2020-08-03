5fa680c092cf7bab1efcb0baddbeb3ba65f5b594
Adrij Shikhar <adrijshikhar85@gmail.com>
2020-08-03 17:38:26 +0530

    Adds grunt file to copy static files

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+// eslint-disable-next-line no-undef
+module.exports = function (grunt) {
+  grunt.initConfig({
+    dir: {
+      src: ".",
+      public: "public",
+    },
+
+    copy: {
+      publish: {
+        files: [
+          {
+            expand: true,
+            dot: true,
+            cwd: "<%= dir.src %>",
+            dest: "<%= dir.public %>",
+            src: ["README.md", "LICENSE", "CHANGELOG.md"],
+          },
+        ],
+      },
+    },
+  });
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.registerTask("default", ["copy"]);
+};
